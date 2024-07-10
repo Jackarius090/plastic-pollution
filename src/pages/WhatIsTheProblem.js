69a7fc178e831a069f7fa38e88c5fa08b65bdd3a
Jack Dickinson <jackdickinson090@hotmail.com>
2024-07-10 12:13:35 +0200

    styling and layout changes

--- src/pages/WhatIsTheProblem.js
+++ src/pages/WhatIsTheProblem.js
@@ -186,79 +186,81 @@ const PlasticInfo = () => {
           </div>
         </div>
       </div>
-      <div id="carouselExampleDark" className="carousel carousel-dark slide p-4">
-        <div className="carousel-indicators">
+      <div className='py-4 px-lg-5 mx-lg-5'>
+        <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
+          <div className="carousel-indicators">
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark"
+              data-bs-slide-to={0}
+              className="active"
+              aria-current="true"
+              aria-label="Slide 1"
+            />
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark"
+              data-bs-slide-to={1}
+              aria-label="Slide 2"
+            />
+            <button
+              type="button"
+              data-bs-target="#carouselExampleDark"
+              data-bs-slide-to={2}
+              aria-label="Slide 3"
+            />
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active" data-bs-interval={10000}>
+              <img
+                src="https://images.unsplash.com/photo-1621451537084-482c73073a0f?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="d-block img-fluid carousel"
+                alt="..."
+              />
+              <div className="carousel-caption d-none d-md-block">
+                <h5 />
+              </div>
+            </div>
+            <div className="carousel-item" data-bs-interval={2000}>
+              <img
+                src="https://images.unsplash.com/photo-1595278069441-2cf29f8005a4?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="d-block img-fluid carousel"
+                alt="..."
+              />
+              <div className="carousel-caption d-none d-md-block">
+                <h5 />
+              </div>
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://images.unsplash.com/flagged/photo-1572213426852-0e4ed8f41ff6?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                className="d-block img-fluid carousel"
+                alt="..."
+              />
+              <div className="carousel-caption d-none d-md-block">
+                <h5 />
+              </div>
+            </div>
+          </div>
           <button
+            className="carousel-control-prev"
             type="button"
             data-bs-target="#carouselExampleDark"
-            data-bs-slide-to={0}
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          />
+            data-bs-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="visually-hidden">Previous</span>
+          </button>
           <button
+            className="carousel-control-next"
             type="button"
             data-bs-target="#carouselExampleDark"
-            data-bs-slide-to={1}
-            aria-label="Slide 2"
-          />
-          <button
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide-to={2}
-            aria-label="Slide 3"
-          />
+            data-bs-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true" />
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active" data-bs-interval={10000}>
-            <img
-              src="https://images.unsplash.com/photo-1621451537084-482c73073a0f?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="d-block mx-auto carousel"
-              alt="..."
-            />
-            <div className="carousel-caption d-none d-md-block">
-              <h5 />
-            </div>
-          </div>
-          <div className="carousel-item" data-bs-interval={2000}>
-            <img
-              src="https://images.unsplash.com/photo-1595278069441-2cf29f8005a4?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="d-block mx-auto carousel"
-              alt="..."
-            />
-            <div className="carousel-caption d-none d-md-block">
-              <h5 />
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://images.unsplash.com/flagged/photo-1572213426852-0e4ed8f41ff6?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              className="d-block mx-auto carousel"
-              alt="..."
-            />
-            <div className="carousel-caption d-none d-md-block">
-              <h5 />
-            </div>
-          </div>
-        </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true" />
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleDark"
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true" />
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
     </div>
   );
